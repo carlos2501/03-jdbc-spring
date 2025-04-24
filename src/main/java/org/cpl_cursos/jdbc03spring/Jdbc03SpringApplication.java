7288121a2b874dc82ffc7600d0c0833e81b53650
CPL <carlos@centropyme.com>
2025-04-24 18:57:11 +0200

    * Configuración del driver en application.properties. * Activación de logs. * Prueba de la conexión mostrando el nombre del DataSource usado por Spring.

--- src/main/java/org/cpl_cursos/jdbc03spring/Jdbc03SpringApplication.java
+++ src/main/java/org/cpl_cursos/jdbc03spring/Jdbc03SpringApplication.java
@@ -1,13 +1,22 @@
 package org.cpl_cursos.jdbc03spring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Jdbc03SpringApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(Jdbc03SpringApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(Jdbc03SpringApplication.class, args);
+        ConfigurableApplicationContext contexto = SpringApplication.run(Jdbc03SpringApplication.class, args);
+        DataSource ds = contexto.getBean(DataSource.class);
+        log.info("Implementación de DataSource -> {}", ds.getClass().getSimpleName());
     }
 
 }
