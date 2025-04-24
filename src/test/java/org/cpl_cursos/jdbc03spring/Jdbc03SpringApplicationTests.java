70169db9479a8476b600de7f0e531f52a2da96f6
CPL <carlos@centropyme.com>
2025-04-24 18:27:34 +0200

    Commit incial. * Sin configuración. * pom.xml con jdbc y PostgreSQL.

---
+++ src/test/java/org/cpl_cursos/jdbc03spring/Jdbc03SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cpl_cursos.jdbc03spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdbc03SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
